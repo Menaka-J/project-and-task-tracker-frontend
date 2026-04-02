9df3a9581e9ab3568cc63f0b7e46ed38ff8df4c8
Menaka J <menakaraman4@gmail.com>
2026-04-03 00:52:59 +0530

    commit

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary-600"></div>
     </div>
   );
 };
