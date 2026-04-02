b40d4e2a575ce0cb511ea7b019dc038945c0e4ff
Menaka J <menakaraman4@gmail.com>
2026-04-03 00:53:35 +0530

    commit

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../common/Navbar';
-import Sidebar from '../common/Sidebar';
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 overflow-x-hidden">
-          <Outlet />
-        </main>
-      </div>
+      <main className="container mx-auto px-4 py-8 max-w-7xl">
+        <Outlet />
+      </main>
     </div>
   );
 };
