f7e0572762922e7326e492b024aea194945f62e0
Menaka J <menakaraman4@gmail.com>
2026-04-11 13:34:31 +0530

    add url

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // Make sure this points to your backend
 // const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
